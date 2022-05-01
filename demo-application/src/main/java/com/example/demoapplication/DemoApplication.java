b6e99c854811350b7f669309ac27bd154bc4df46
pram <pram@attale.com>
2022-05-01 11:09:37 +0100

    Checking intellij connectivity

--- demo-application/src/main/java/com/example/demoapplication/DemoApplication.java
+++ demo-application/src/main/java/com/example/demoapplication/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
   @RequestMapping("/")
   public String home() {
-     return "Hello Everybody! Again + Again + Again";
+     return "Hello Everybody! Again + Again + Again - Things";
   }
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
