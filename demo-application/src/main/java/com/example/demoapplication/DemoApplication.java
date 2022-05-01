7cef2f24c250644e1c49f4130a704d420e11c703
pram <pram@attale.com>
2022-05-01 10:34:35 +0100

    Adding example project

---
+++ demo-application/src/main/java/com/example/demoapplication/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+  @RequestMapping("/")
+  public String home() {
+     return "Hello Everybody! Again + Again + Again";
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
